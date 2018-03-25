304dc35d95c474732be74f08a9ef193c6bd009f5
Leshii2902 <aleks-gricen@yandex.ru>
2018-03-25 21:00:42 +0300

    добавление и удаление поста

--- src/app/actions/topLikesPostsActions.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export function fetchTopLikesPost() {
-    return {
-        type: 'FETCH_TOP_LIKES_POSTS',
-        payload: axios.get('/api/posts/top-likes')
-    }
-}
--- src/app/actions/topViewsPostsActions.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export function fetchTopViewsPost() {
-    return {
-        type: 'FETCH_TOP_VIEWS_POSTS',
-        payload: axios.get('/api/posts/top-views')
-    }
-}

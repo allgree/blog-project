c349c9d37a73b8573b64524f250499033a6a7279
Leshii2902 <aleks-gricen@yandex.ru>
2018-08-15 19:57:10 +0300

    изменения изображений, блокировка действия по умолчанию у формы поиска

--- src/app/components/Content/forms/SearchForm.js
+++ src/app/components/Content/forms/SearchForm.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 export default class SearchForm extends React.Component {
+    constructor() {
+        super(...arguments);
+        this.noSubmit = this.noSubmit.bind(this);
+    }
+
+    noSubmit(e) {
+        e.preventDefault();
+    }
+
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit} className="search_form">
+            <form onSubmit={this.noSubmit} className="search_form">
                 <input type="text"
                        id="search"
                        className="input_custom"
